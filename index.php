fe739b0bbf4e8633f4ac03d5d935f68ca19e9a2e
Lucas Ramos <lucas_ramos1994@hotmail.com>
2023-09-15 13:59:50 -0300

    adiciona pontos de teste

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 $system = new SolarSystemSvg(300, 300);
 
 $system->addPlanet(10, 80, true);
-$system->addPlanet(5, 55, true);
-$system->addPlanet(3, 35, true);
-$system->addPlanet(5, 150, true);
+// $system->addPlanet(5, 55, true);
+// $system->addPlanet(3, 35, true);
+// $system->addPlanet(5, 150, true);
 
 
 ?>
